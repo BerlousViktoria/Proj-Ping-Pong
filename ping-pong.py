c880d52e798e2a52f07afcb19ae3d619cc4fb5f5
Berlous Viktoria <131599267+BerlousViktoria@users.noreply.github.com>
2023-05-14 18:00:17 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -31,6 +31,14 @@ racket1 = Player("racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("racket.png", 520, 200, 4, 50, 150)
 ball = GameSprite("tennis_ball.png", 200, 200, 4, 50, 50)
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render("PLAYER 1 LOSE!", True, (180, 0, 0))
+lose2 = font.render("PLAYER 2 LOSE!", True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 while game:
   for e in event.get():
     if e.type == QUIT:
@@ -42,7 +50,23 @@ while game:
         racket2.update_r()
         ball.rect_x += speed_x
         ball.rect_y += speed_y
+        
+      if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball)
+        speed_x *= -1
+        speed_y *= 1
+        
+      if ball.rect.x < 0 or ball.rect.x > 600:
+        speed_y *= -1
+       
+      if ball.rect.x < 0:
+            window.blit(lose1, (200, 200))
+            
+      if ball.rect.x > win_width:
+        finish = True
+        window.blit(lose2, (200, 200))
   
+
+display.update()
   
 game = True
 finish = False
